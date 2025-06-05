5db99702914ef02e3313e7a351276d9224c51bb9
bellinel <artursimoncik@gmail.com>
2025-06-05 15:47:30 +0300

    Update project files and dependencies

--- send_message.py
+++ send_message.py
@@ -1,13 +1,22 @@
+import os
 import aiohttp
 import asyncio
+from dotenv import load_dotenv
+
+load_dotenv()
+
+IDISTANSE = os.getenv("IDISTANSE")
+API_KEY = os.getenv("API_KEY")
+GROUP_ID = os.getenv("GROUP_ID")
+
 
 async def send_whatsapp_message(text):
-    # url = "https://7105.api.greenapi.com/waInstance7105255577/sendMessage/259b7a7d620742d1babbe846294245907c353e7490eb46d5b3"
-    url = "https://7105.api.greenapi.com/waInstance7105218511/sendMessage/fb4cbfa4f35d4141b208cf56b8da429680dc269c41464b3b97"
+    url = f"https://7105.api.greenapi.com/waInstance{IDISTANSE}/sendMessage/{API_KEY}"
+    
 
     payload = {
-        "chatId": "120363399527013430@g.us",
-        # 'chatId': '375257224153@c.us',
+        
+        'chatId': GROUP_ID,
         "message": f'{text}'
     }
 
@@ -21,7 +30,7 @@ async def send_whatsapp_message(text):
             print(response_text)
 
 
-
+asyncio.run(send_whatsapp_message("test"))
 
 
 
