6c1ff6a0c36c3a47dd8c322935a9a7e389e5ea25
bellinel <154801505+bellinel@users.noreply.github.com>
2025-06-11 18:09:23 +0300

    Update olx.py

--- olx.py
+++ olx.py
@@ -7,6 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import shutil
 
 from database.orm import get_site_url_olx, update_site_url_olx
 
@@ -22,6 +23,7 @@ async def olx_parse():
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-dev-tools")
     options.add_argument("--remote-debugging-port=0")
+    
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36")
     try:
         driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version="135").install()), options=options)
@@ -30,18 +32,17 @@ async def olx_parse():
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
         # apartment = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "listing-grid-container.css-d4ctjd")))
         apartment = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.css-l9drzq")))
-        name = apartment.find_element(By.CLASS_NAME, "css-1g61gc2").text
-        price = apartment.find_element(By.CLASS_NAME, "css-uj7mm0").text.split(".")[0]
+        
     
             
         url = apartment.find_element(By.CLASS_NAME, "css-u2ayx9").find_element(By.TAG_NAME, "a").get_attribute("href")
         current_url = await get_site_url_olx()
 
         if str(url) == str(current_url):
-            
+            print("URL совпадает OLX")
             return None
 
-        result = f"{name}\n{price}\n{url}\n"
+        result = f"{url}"
         await update_site_url_olx(url)
         
         
@@ -50,5 +51,6 @@ async def olx_parse():
     finally:
         try:
             driver.quit()
+            
         except:
             pass
