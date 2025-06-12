b4e788cd7c24bdf79c7441d8b77c29b07fe0316f
bellinel <154801505+bellinel@users.noreply.github.com>
2025-06-12 11:29:56 +0300

    Update olx.py

--- olx.py
+++ olx.py
@@ -14,7 +14,7 @@ from database.orm import get_site_url_olx, update_site_url_olx
 
 async def olx_parse():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
@@ -31,22 +31,21 @@ async def olx_parse():
         
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
         # apartment = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "listing-grid-container.css-d4ctjd")))
-        apartment = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.css-l9drzq")))
+        apartment = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[data-cy='l-card']")))
         
     
             
         url = apartment.find_element(By.CLASS_NAME, "css-u2ayx9").find_element(By.TAG_NAME, "a").get_attribute("href")
-        current_url = await get_site_url_olx()
-
-        if str(url) == str(current_url):
-            print("URL совпадает OLX")
-            return None
+        
 
         result = f"{url}"
-        await update_site_url_olx(url)
+        new_url = await update_site_url_olx(url)
+        if new_url:
+            return result
+        else:
+            return None
         
         
-        return result
     
     finally:
         try:
