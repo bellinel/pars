4bbdae4fb1c9c0a003927b222f81058519b8453d
bellinel <154801505+bellinel@users.noreply.github.com>
2025-06-13 17:18:25 +0300

    Update engine.py

--- database/engine.py
+++ database/engine.py
@@ -67,7 +67,7 @@ class OlxId(Base):
     __tablename__ = "olx_id"
 
     id = Column(Integer, primary_key=True)
-    site_url = Column(String, nullable=False)
+    site_url = Column(String, nullable=False, unique=True)
 
 
 
